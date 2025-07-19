1f6e119afc4dbac73717e80172773b2d54e3e6bb
Khun Tu Pi <bwarpay.bp8@gmail.com>
2025-07-19 09:50:23 +0630

    connect view and controller

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
     public function index() {
-        return "Controller Index";
+        $data = [
+            ["title" => "Article Title One"],
+            ["title" => "Two Title Two"]
+        ];
+        return view("articles.index",[ "articles" => $data]);
+
     }
 
     public function detail($id) {
